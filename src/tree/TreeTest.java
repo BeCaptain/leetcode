6a7efbc253c25cae27272995e7dcd8c83aadba6c
BeCaptain <xzx991004@163.com>
2023-04-18 11:29:04 +0800

    TreeTest

---
+++ src/tree/TreeTest.java
@@ -0,0 +1,17 @@
+package tree;
+
+/**
+ * @author Xie Zexian
+ * @description the test class of tree
+ * @createTime 2023/4/17 15:39
+ */
+public class TreeTest {
+    public static void main(String[] args) {
+        TreeNode root = BinaryTreeUtils.buildBinaryTree(new Integer[]{3, 9, 20, null, null, 15, 7});
+    }
+
+
+}
+
+
+
